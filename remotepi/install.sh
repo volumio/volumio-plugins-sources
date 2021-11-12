1f9af85158e3d8b1a64044e24e3eda1c5cb3cebc
gvolt <jem_7@web.de>
2021-11-12 21:38:08 +0100

    Fix "onoff" module issue

--- remotepi/install.sh
+++ remotepi/install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+echo "Installing build-essential"
+apt-get update
+apt-get -y install build-essential
+cd $(dirname $0)
+
+echo "Installing module \"onoff\""
+npm install onoff@^6.0.0
+
 #required to end the plugin install
 echo "plugininstallend"
--- remotepi/uninstall.sh
+++ remotepi/uninstall.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Removing dependencies"
+sudo apt-get -y purge --auto-remove build-essential
+
 echo "Done"
 echo "pluginuninstallend"
