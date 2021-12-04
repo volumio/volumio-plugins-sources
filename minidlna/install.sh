f6e070dc8a593c41fa5237d46764cb93ce8623c4
gvolt <jem_7@web.de>
2021-12-04 16:39:22 +0100

    miniDLNA 3.1.10

--- minidlna/install.sh
+++ minidlna/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Installing MiniDLNA"
-sudo apt-get update
-sudo apt-get -y install minidlna
-sudo systemctl disable minidlna.service
-sudo systemctl stop minidlna.service
-sudo rm /etc/minidlna.conf
-sudo rm /data/minidlna.conf
+apt-get update
+apt-get -y install minidlna
+systemctl disable minidlna.service
+systemctl stop minidlna.service
+rm /etc/minidlna.conf
+rm /data/minidlna.conf
 
 minidlnad=$(whereis -b minidlnad | cut -d ' ' -f2)
 echo "Creating systemd unit /etc/systemd/system/minidlna.service"
-sudo echo "[Unit]
+echo "[Unit]
 Description=MiniDLNA lightweight DLNA/UPnP-AV server
 Documentation=man:minidlnad(1) man:minidlna.conf(5)
 After=local-fs.target remote-fs.target nss-lookup.target network.target
@@ -29,13 +29,13 @@ ExecStart=$minidlnad -f \$CONFIGFILE -P /run/minidlna/minidlna.pid \$DAEMON_OPTS
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/minidlna.service
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
 echo "Setting values for \"network_interface\" and \"model_number\" in /data/plugins/music_service/minidlna/config.json"
 sed -i "/\"value\": \"eth0,wlan0\"/s/\"eth0,wlan0\"/\"$(ip -o link show | grep -v ": lo:" | cut -s -d":" -f2 | cut -s -d" " -f2 | tr "[:cntrl:]" "," | head --bytes -1)\"/1" /data/plugins/music_service/minidlna/config.json
 sed -i "/\"value\": \"Volumio Edition\"/s/\"Volumio Edition\"/\"$($minidlnad -V | tr -d "[:cntrl:]")\"/1" /data/plugins/music_service/minidlna/config.json
 
 echo "Setting permissions to MiniDLNA folders"
-sudo chown -R volumio:volumio /var/cache/minidlna/
+chown -R volumio:volumio /var/cache/minidlna/
 
 echo "plugininstallend"
--- minidlna/uninstall.sh
+++ minidlna/uninstall.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Removing dependencies"
-sudo apt-get -y purge --auto-remove minidlna
+apt-get -y purge --auto-remove minidlna
 
 echo "Deleting /data/minidlna.conf"
-sudo rm /data/minidlna.conf
+rm /data/minidlna.conf
 
 echo "Deleting systemd unit /etc/systemd/system/minidlna.service"
-sudo rm /etc/systemd/system/minidlna.service
-sudo systemctl daemon-reload
+rm /etc/systemd/system/minidlna.service
+systemctl daemon-reload
 
 echo "Done"
 echo "pluginuninstallend"
