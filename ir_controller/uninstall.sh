9d57018fef96c624813d26dab3082d0e6e9c1f34
gvolt <jem_7@web.de>
2022-01-06 23:52:29 +0100

    IR Controller 3.4.7

--- ir_controller/uninstall.sh
+++ ir_controller/uninstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Removing dependencies"
-sudo rm /etc/lirc/lircrc
-sudo apt-get -y purge --auto-remove lirc
+rm /etc/lirc/lircrc
+apt-get -y purge --auto-remove lirc
 
 echo "Removing folder for custom LIRC configurations"
-sudo rm -r /data/INTERNAL/ir_controller
+rm -r /data/INTERNAL/ir_controller
 
 echo "Done"
 echo "pluginuninstallend"
