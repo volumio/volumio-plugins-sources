52b738b7422795dcb58250284c0730585c4cb98c
Rachid Groeneveld <Saiyato@users.noreply.github.com>
2022-02-08 09:59:52 +0100

    Use code function instead of custom logic
    Also cleaned up a lot of whitespace and obsolete lines/comments.

--- lms/index.js
+++ lms/index.js
@@ -2,11 +2,8 @@
 
 var exec = require('child_process').exec;
 var fs = require('fs-extra');
-var ifconfig = require('wireless-tools/ifconfig');
-var ip = require('ip');
 var libQ = require('kew');
-//var net = require('net');
-var currentIp = '';
+var selfIP = '';
 
 // Define the ControllerLMS class
 module.exports = ControllerLMS;
@@ -14,12 +11,10 @@ module.exports = ControllerLMS;
 function ControllerLMS(context) 
 {
 	var self = this;
-
 	this.context = context;
 	this.commandRouter = this.context.coreCommand;
 	this.logger = this.context.logger;
 	this.configManager = this.context.configManager;
-
 };
 
 ControllerLMS.prototype.onVolumioStart = function()
@@ -55,7 +50,6 @@ ControllerLMS.prototype.onStop = function() {
 ControllerLMS.prototype.stop = function() {
 	var self = this;
 	var defer = libQ.defer();
-
 	self.stopService('logitechmediaserver')
 	.then(function(edefer)
 	{
@@ -73,9 +67,12 @@ ControllerLMS.prototype.stop = function() {
 ControllerLMS.prototype.onStart = function() {
 	var self = this;
 	var defer = libQ.defer();
-
 	self.restartService('logitechmediaserver', true)
 	.then(function(edefer)
+	{
+		self.selfIP = self.commandRouter.getCachedIPAddresses();
+	})
+	.then(function(fdefer)
 	{
 		defer.resolve();
 	})
@@ -113,35 +110,29 @@ ControllerLMS.prototype.getUIConfig = function() {
     var self = this;
 	var defer = libQ.defer();    
     var lang_code = this.commandRouter.sharedVars.get('language_code');
-
-	self.getCurrentIP();
 	self.logger.info("Loaded the previous config.");
-	
 	self.commandRouter.i18nJson(__dirname+'/i18n/strings_' + lang_code + '.json',
 		__dirname + '/i18n/strings_en.json',
 		__dirname + '/UIConfig.json')
     .then(function(uiconf)
     {
-		var consoleUrl = 'http://' + currentIp + ':9000';
-		
+		self.logger.info("[LMS] Loading configuration...");
+		let consoleUrl = `http://${self.selfIP['eth0']}:9000`;
+		self.logger.info(`[LMS] Console URL: ${consoleUrl}`);
 		uiconf.sections[0].content[0].onClick.url = consoleUrl;
-		
 		defer.resolve(uiconf);
 	})
 	.fail(function()
 	{
 		defer.reject(new Error());
 	});
-
 	return defer.promise;
 };
 
 ControllerLMS.prototype.setUIConfig = function(data) {
 	var self = this;
-	
 	self.logger.info("Updating UI config");
 	var uiconf = fs.readJsonSync(__dirname + '/UIConfig.json');
-	
 	return libQ.resolve();
 };
 
@@ -162,24 +153,20 @@ ControllerLMS.prototype.restartService = function (serviceName, boot)
 {
 	var self = this;
 	var defer=libQ.defer();
-
 	var command = "/usr/bin/sudo /bin/systemctl restart " + serviceName;
-	
 	exec(command, {uid:1000,gid:1000}, function (error, stdout, stderr) {
 		if (error !== null) {
 			self.commandRouter.pushConsoleMessage('The following error occurred while starting ' + serviceName + ': ' + error);
 			self.commandRouter.pushToastMessage('error', "Restart failed", "Restarting " + serviceName + " failed with error: " + error);
-			defer.reject();
+			defer.reject(new Error('Unable to restart service LMS'));
 		}
 		else {
 			self.commandRouter.pushConsoleMessage(serviceName + ' started');
 			if(boot == false)
 				self.commandRouter.pushToastMessage('success', "Restarted " + serviceName, "Restarted " + serviceName + " for the changes to take effect.");
-			
 			defer.resolve();
 		}
 	});
-
 	return defer.promise;
 };
 
@@ -187,14 +174,12 @@ ControllerLMS.prototype.stopService = function (serviceName)
 {
 	var self = this;
 	var defer=libQ.defer();
-
 	var command = "/usr/bin/sudo /bin/systemctl stop " + serviceName;
-	
 	exec(command, {uid:1000,gid:1000}, function (error, stdout, stderr) {
 		if (error !== null) {
 			self.commandRouter.pushConsoleMessage('The following error occurred while stopping ' + serviceName + ': ' + error);
 			self.commandRouter.pushToastMessage('error', "Stopping service failed", "Stopping " + serviceName + " failed with error: " + error);
-			defer.reject();
+			defer.reject(new Error('Unable to stop service LMS'));
 		}
 		else {
 			self.commandRouter.pushConsoleMessage(serviceName + ' stopped');
@@ -202,37 +187,5 @@ ControllerLMS.prototype.stopService = function (serviceName)
 			defer.resolve();
 		}
 	});
-
 	return defer.promise;
-};
-
-ControllerLMS.prototype.getCurrentIP = function () {
-    var self = this;
-    var defer = libQ.defer();
-	var ipaddr = '';
-	
-    ifconfig.status('wlan0', function(err, status) 
-	{
-        if (status != undefined)
-		{
-			// Omit the hotspot address
-            if (status.ipv4_address != undefined && status.ipv4_address != '192.168.211.1')
-			{
-                currentIp = status.ipv4_address;
-                defer.resolve(ipaddr);
-            } 
-			else 
-			{
-                currentIp = ip.address();
-                defer.resolve(ipaddr);
-            }
-        }
-		else
-		{
-			// Try again, but now only ethernet
-			currentIp = ip.address();
-			defer.resolve(ipaddr);
-		}
-    });
-    return defer.promise;
 };
