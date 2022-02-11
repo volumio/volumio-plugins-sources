7eda0be69ddd41f24078ca790377b69f3d028ae1
gvolt <jem_7@web.de>
2022-02-11 22:35:42 +0100

    miniDLNA 3.1.12

--- minidlna/install.sh
+++ minidlna/install.sh
@@ -6,10 +6,12 @@ exit_cleanup() {
   if [ "$?" -ne 0 ]; then
     echo "Plugin failed to install!"
     echo "Cleaning up..."
-    . ."$PLUGIN_DIR"/uninstall.sh | grep -v "pluginuninstallend"
     if [ -d "$PLUGIN_DIR" ]; then
+      . ."$PLUGIN_DIR"/uninstall.sh | grep -v "pluginuninstallend"
       echo "Removing plugin directory $PLUGIN_DIR"
       rm -rf "$PLUGIN_DIR"
+    else
+      echo "Plugin directory could not be found: Cleaning up failed."
     fi
   fi
 
@@ -17,6 +19,9 @@ exit_cleanup() {
   echo "plugininstallend"
 }
 trap "exit_cleanup" EXIT
+
+echo "Completing \"UIConfig.json\""
+sed -i "s/\${plugin_type}/$(grep "\"plugin_type\":" "$PLUGIN_DIR"/package.json | cut -d "\"" -f 4)/" "$PLUGIN_DIR"/UIConfig.json || { echo "Completing \"UIConfig.json\" failed"; exit 1; }
 
 echo "Installing MiniDLNA"
 apt-get update
@@ -26,7 +31,7 @@ systemctl stop minidlna.service
 rm /etc/minidlna.conf
 rm /data/minidlna.conf
 
-MINIDLNAD=$(whereis -b minidlnad | cut -d ' ' -f2)
+MINIDLNAD=$(whereis -b minidlnad | cut -d ' ' -f 2)
 echo "Creating systemd unit /etc/systemd/system/minidlna.service"
 echo "[Unit]
 Description=MiniDLNA lightweight DLNA/UPnP-AV server
@@ -43,15 +48,15 @@ EnvironmentFile=-/etc/default/minidlna
 
 RuntimeDirectory=minidlna
 PIDFile=/run/minidlna/minidlna.pid
-ExecStart="$MINIDLNAD" -f \$CONFIGFILE -P /run/minidlna/minidlna.pid \$DAEMON_OPTS
+ExecStart=$MINIDLNAD -f \$CONFIGFILE -P /run/minidlna/minidlna.pid \$DAEMON_OPTS
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/minidlna.service || { echo "Creating systemd unit /etc/systemd/system/minidlna.service failed"; exit 1; }
 systemctl daemon-reload
 
-echo "Setting values for \"network_interface\" and \"model_number\" in /data/plugins/music_service/minidlna/config.json"
-sed -i "/\"value\": \"eth0,wlan0\"/s/\"eth0,wlan0\"/\"$(ip -o link show | grep -v ": lo:" | cut -s -d":" -f2 | cut -s -d" " -f2 | tr "[:cntrl:]" "," | head --bytes -1)\"/1" /data/plugins/music_service/minidlna/config.json
-sed -i "/\"value\": \"Volumio Edition\"/s/\"Volumio Edition\"/\"$("$MINIDLNAD" -V | tr -d "[:cntrl:]")\"/1" /data/plugins/music_service/minidlna/config.json
+echo "Setting values for \"network_interface\" and \"model_number\" in ""$PLUGIN_DIR""/config.json"
+sed -i "/\"value\": \"eth0,wlan0\"/s/\"eth0,wlan0\"/\"$(ip -o link show | grep -v ": lo:" | cut -s -d ":" -f 2 | cut -s -d " " -f 2 | tr "[:cntrl:]" "," | head --bytes -1)\"/1" "$PLUGIN_DIR"/config.json
+sed -i "/\"value\": \"Volumio Edition\"/s/\"Volumio Edition\"/\"$("$MINIDLNAD" -V | tr -d "[:cntrl:]")\"/1" "$PLUGIN_DIR"/config.json
 
 echo "Setting permissions to MiniDLNA folders"
 chown -R volumio:volumio /var/cache/minidlna/ || { echo "Setting permissions to MiniDLNA folders failed"; exit 1; }
