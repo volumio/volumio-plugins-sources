6ecccf0abacbaefd7a1667c45eba7e1b8a6ebfab
Rachid Groeneveld <Saiyato@users.noreply.github.com>
2022-03-23 17:20:33 +0100

    Fix missing template issue
    Also tidied up the code

--- squeezelite/index.js
+++ squeezelite/index.js
@@ -14,22 +14,18 @@ module.exports = ControllerSqueezelite;
 function ControllerSqueezelite(context) 
 {
 	var self = this;
-
 	this.context = context;
 	this.commandRouter = this.context.coreCommand;
 	this.logger = this.context.logger;
 	this.configManager = this.context.configManager;
-
 };
 
 ControllerSqueezelite.prototype.onVolumioStart = function()
 {
 	var self = this;
-	self.logger.info("Squeezelite initiated");
-	
+	self.logger.info("[Squeezelite] Initiated plugin");	
 	this.configFile = this.commandRouter.pluginManager.getConfigurationFile(this.context, 'config.json');
-	self.getConf(this.configFile);
-	
+	self.getConf(this.configFile);	
 	return libQ.resolve();	
 };
 
@@ -42,7 +38,6 @@ ControllerSqueezelite.prototype.getConfigurationFiles = function()
 ControllerSqueezelite.prototype.onStop = function() {
 	var self = this;
 	var defer = libQ.defer();
-
 	self.stopService('squeezelite')
 	.then(function(edefer)
 	{
@@ -53,14 +48,12 @@ ControllerSqueezelite.prototype.onStop = function() {
 		self.commandRouter.pushToastMessage('error', "Stopping failed", "Could not stop the Squeezelite plugin in a fashionable manner, error: " + e);
 		defer.reject(new error());
 	});
-
 	return defer.promise;
 };
 
 ControllerSqueezelite.prototype.stop = function() {
 	var self = this;
 	var defer = libQ.defer();
-
 	self.stopService('squeezelite')
 	.then(function(edefer)
 	{
@@ -71,14 +64,12 @@ ControllerSqueezelite.prototype.stop = function() {
 		self.commandRouter.pushToastMessage('error', "Stopping failed", "Could not stop the Squeezelite plugin in a fashionable manner, error: " + e);
 		defer.reject(new error());
 	});
-
 	return defer.promise;
 };
 
 ControllerSqueezelite.prototype.onStart = function() {
 	var self = this;
 	var defer = libQ.defer();
-
 	self.restartService('squeezelite', true)
 	.then(function(edefer)
 	{
@@ -87,25 +78,21 @@ ControllerSqueezelite.prototype.onStart = function() {
 	.fail(function(e)
 	{
 		self.commandRouter.pushToastMessage('error', "Startup failed", "Could not start the Squeezelite plugin in a fashionable manner.");
-		self.logger.info("Could not start the Squeezelite plugin in a fashionable manner.");
+		self.logger.info("[Squeezelite] Could not start the Squeezelite plugin in a fashionable manner.");
 		defer.reject(new error());
 	});
-
 	return defer.promise;
 };
 
 ControllerSqueezelite.prototype.onRestart = function() 
 {
-	// Do nothing
-	self.logger.info("performing onRestart action");
-	
+	// self.logger.info("performing onRestart action");	
 	var self = this;
 };
 
 ControllerSqueezelite.prototype.onInstall = function() 
 {
-	self.logger.info("performing onInstall action");
-	
+	// self.logger.info("performing onInstall action");	
 	var self = this;
 };
 
@@ -118,10 +105,7 @@ ControllerSqueezelite.prototype.getUIConfig = function() {
     var self = this;
 	var defer = libQ.defer();    
     var lang_code = this.commandRouter.sharedVars.get('language_code');
-
-	self.getConf(this.configFile);
-	self.logger.info("Loaded the previous config.");
-	
+	self.getConf(this.configFile);	
 	var cards = [];
 	cards.push({ hwAddress: "default", name: "ALSA default", description: "System-wide default audio device" });
 	var stdOut = execSync("/opt/squeezelite -l | grep '^\\s*[a-z]\\{2,10\\}:[A-Z]*=[a-z]*[A-Z,]\\{0,\\}\\(=[0-1]\\)\\{0,2\\}'").toString().split(/\r?\n/);
@@ -133,31 +117,27 @@ ControllerSqueezelite.prototype.getUIConfig = function() {
 			cards.push({ hwAddress: cardObj[0].toString().trim(), name: cardObj[1].toString().trim(), description: cardObj[2].toString().trim() });
 		}
 	}
-	//self.logger.info('Cards: ' + JSON.stringify(cards));
+	//self.logger.info('[Squeezelite] Cards: ' + JSON.stringify(cards));
 	var seconds = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-
 	self.commandRouter.i18nJson(__dirname+'/i18n/strings_' + lang_code + '.json',
 		__dirname + '/i18n/strings_en.json',
 		__dirname + '/UIConfig.json')
     .then(function(uiconf)
     {
-		self.logger.info("## populating UI...");
-		
+		self.logger.info("[Squeezelite] Populating UI for configuration...");		
 		uiconf.sections[0].content[0].value = self.config.get('enabled');
 		uiconf.sections[0].content[1].value = self.config.get('name');
-		self.logger.info("1/2 Squeezelite settings sections loaded");
+		self.logger.info("[Squeezelite] 1/2 settings sections loaded");
 		
 		for (var soundcard in cards)
 		{
-			//self.logger.info('Card to add: ' + cards[soundcard].name + ' BLOB: ' + JSON.stringify(cards[soundcard]));			
+			//self.logger.info('[Squeezelite] Card to add: ' + cards[soundcard].name + ' BLOB: ' + JSON.stringify(cards[soundcard]));			
 			var oLabel = cards[soundcard].hwAddress == "default" ? '[ALSA] ' + cards[soundcard].description
 			: '[(' + cards[soundcard].hwAddress.substring(0, cards[soundcard].hwAddress.indexOf(":")) + ') ' + cards[soundcard].name.substring(0, cards[soundcard].name.indexOf(",")) + '] ' + cards[soundcard].description;
-			
 			self.configManager.pushUIConfigParam(uiconf, 'sections[1].content[0].options', {
 				value: cards[soundcard].hwAddress,
 				label: oLabel
-			});
-			
+			});			
 			if(self.config.get('output_device') == cards[soundcard].hwAddress)
 			{
 				uiconf.sections[1].content[0].value.value = cards[soundcard].hwAddress;
@@ -180,10 +160,7 @@ ControllerSqueezelite.prototype.getUIConfig = function() {
 		}		
 		uiconf.sections[1].content[2].value = self.config.get('alsa_params');
 		uiconf.sections[1].content[3].value = self.config.get('extra_params');
-		self.logger.info("2/2 Squeezelite settings sections loaded");
-
-		self.logger.info("Populated config screen.");
-		
+		self.logger.info("[Squeezelite] 2/2 settings sections loaded");		
 		defer.resolve(uiconf);
 	})
 	.fail(function()
@@ -196,18 +173,14 @@ ControllerSqueezelite.prototype.getUIConfig = function() {
 
 ControllerSqueezelite.prototype.setUIConfig = function(data) {
 	var self = this;
-	
-	self.logger.info("Updating UI config");
-	var uiconf = fs.readJsonSync(__dirname + '/UIConfig.json');
-	
+	var uiconf = fs.readJsonSync(__dirname + '/UIConfig.json');	
 	return libQ.resolve();
 };
 
 ControllerSqueezelite.prototype.getConf = function(configFile) {
 	var self = this;
-	this.config = new (require('v-conf'))()
-	this.config.loadFile(configFile)
-	
+	this.config = new (require('v-conf'))();
+	this.config.loadFile(configFile);
 	return libQ.resolve();
 };
 
@@ -222,13 +195,10 @@ ControllerSqueezelite.prototype.updateSqueezeliteServiceConfig = function (data)
 {
 	var self = this;
 	var defer = libQ.defer();
-	
 	self.config.set('enabled', data['enabled']);
 	self.config.set('name', data['name']);
-	
-	self.logger.info("Successfully updated Squeezelite service configuration");
-
-	self.constructUnit(__dirname + "/unit/squeezelite.unit-template", __dirname + "/unit/squeezelite.service")
+	self.logger.info("[Squeezelite] Successfully updated Squeezelite service configuration");
+	self.constructUnit(__dirname + "/unit/squeezelite.service-template", __dirname + "/unit/squeezelite.service")
 	.then(function(stopIfNeeded){
 		if(self.config.get('enabled') != true)
 		{
@@ -244,7 +214,6 @@ ControllerSqueezelite.prototype.updateSqueezeliteServiceConfig = function (data)
 			});
 		}
 	});
-	
 	return defer.promise;
 };
 
@@ -252,15 +221,12 @@ ControllerSqueezelite.prototype.updateSqueezeliteAudioConfig = function (data)
 {
 	var self = this;
 	var defer = libQ.defer();
-	
 	self.config.set('output_device', data['output_device'].value);
 	self.config.set('soundcard_timeout', data['soundcard_timeout'].value);
 	self.config.set('alsa_params', data['alsa_params']);
 	self.config.set('extra_params', data['extra_params']);
-	
-	self.logger.info("Successfully updated Squeezelite audio configuration");
-
-	self.constructUnit(__dirname + "/unit/squeezelite.unit-template", __dirname + "/squeezelite.service")
+	self.logger.info("[Squeezelite] Successfully updated Squeezelite audio configuration");
+	self.constructUnit(__dirname + "/unit/squeezelite.service-template", __dirname + "/squeezelite.service")
 	.then(function(stopIfNeeded){
 		if(self.config.get('enabled') != true)
 		{
@@ -276,7 +242,6 @@ ControllerSqueezelite.prototype.updateSqueezeliteAudioConfig = function (data)
 			});
 		}
 	});
-	
 	return defer.promise;
 };
 
@@ -284,11 +249,9 @@ ControllerSqueezelite.prototype.restartService = function (serviceName, boot)
 {
 	var self = this;
 	var defer = libQ.defer();
-
 	if(self.config.get('enabled'))
 	{
 		var command = "/usr/bin/sudo /bin/systemctl restart " + serviceName;
-		
 		self.reloadService(serviceName)
 		.then(function(restart){
 			exec(command, {uid:1000,gid:1000}, function (error, stdout, stderr) {
@@ -309,10 +272,9 @@ ControllerSqueezelite.prototype.restartService = function (serviceName, boot)
 	}
 	else
 	{
-		self.logger.info("Not starting " + serviceName + "; it's not enabled.");
+		self.logger.info("[Squeezelite] Not starting " + serviceName + "; it's not enabled.");
 		defer.resolve();
 	}
-
 	return defer.promise;
 };
 
@@ -320,7 +282,6 @@ ControllerSqueezelite.prototype.reloadService = function (serviceName)
 {
 	var self = this;
 	var defer = libQ.defer();
-
 	var command = "/usr/bin/sudo /bin/systemctl daemon-reload";
 	exec(command, {uid:1000,gid:1000}, function (error, stdout, stderr) {
 		if (error !== null) {
@@ -334,7 +295,6 @@ ControllerSqueezelite.prototype.reloadService = function (serviceName)
 			defer.resolve();
 		}
 	});
-		
 	return defer.promise;
 };
 
@@ -342,9 +302,7 @@ ControllerSqueezelite.prototype.stopService = function (serviceName)
 {
 	var self = this;
 	var defer = libQ.defer();
-
 	var command = "/usr/bin/sudo /bin/systemctl stop " + serviceName;
-	
 	exec(command, {uid:1000,gid:1000}, function (error, stdout, stderr) {
 		if (error !== null) {
 			self.commandRouter.pushConsoleMessage('The following error occurred while stopping ' + serviceName + ': ' + error);
@@ -357,7 +315,6 @@ ControllerSqueezelite.prototype.stopService = function (serviceName)
 			defer.resolve();
 		}
 	});
-
 	return defer.promise;
 };
 
@@ -365,7 +322,6 @@ ControllerSqueezelite.prototype.constructUnit = function(unitTemplate, unitFile)
 {
 	var self = this;
 	var defer = libQ.defer();
-	
 	var replacementDictionary = [
 		{ placeholder: "${NAME}", replacement: self.config.get('name') },
 		{ placeholder: "${OUTPUT_DEVICE}", replacement: self.config.get('output_device') },
@@ -373,7 +329,6 @@ ControllerSqueezelite.prototype.constructUnit = function(unitTemplate, unitFile)
 		{ placeholder: "${ALSA_PARAMS}", replacement: self.config.get('alsa_params') },
 		{ placeholder: "${EXTRA_PARAMS}", replacement: self.config.get('extra_params') }
 	];
-	
 	for (var rep in replacementDictionary)
 	{
 		if(replacementDictionary[rep].replacement == undefined || replacementDictionary[rep].replacement == 'undefined')
@@ -395,7 +350,6 @@ ControllerSqueezelite.prototype.constructUnit = function(unitTemplate, unitFile)
 				replacementDictionary[rep].replacement = "-a " + replacementDictionary[rep].replacement;
 		}
 	}
-	
 	self.replaceStringsInFile(unitTemplate, unitFile, replacementDictionary)
 	.then(function(resolve){
 		self.restartService('squeezelite', false);
@@ -404,7 +358,6 @@ ControllerSqueezelite.prototype.constructUnit = function(unitTemplate, unitFile)
 	.fail(function(resolve){
 		defer.reject();
 	});
-	
 	return defer.promise;
 }
 
@@ -417,27 +370,23 @@ ControllerSqueezelite.prototype.replaceStringsInFile = function(sourceFilePath, 
 		if (err) {
 			defer.reject(new Error(err));
 		}
-		
 		var tmpConf = data;
 		for (var rep in replacements)
 		{
 			tmpConf = tmpConf.replace(replacements[rep].placeholder, replacements[rep].replacement);
-			//self.logger.info('Replacing ' + replacements[rep].placeholder + " with " + replacements[rep].replacement);
+			// self.logger.info('[Squeezelite] Replacing ' + replacements[rep].placeholder + " with " + replacements[rep].replacement);
 		}
-		
 		fs.writeFile(destinationFilePath, tmpConf, 'utf8', function (err) {
-                if (err)
-				{
-					self.commandRouter.pushConsoleMessage('Could not write the script with error: ' + err);
-                    defer.reject(new Error(err));
-				}
-                else 
-				{
-					self.logger.info('New unit-file created!');
-					defer.resolve();
-				}
+			if (err)
+			{
+				self.commandRouter.pushConsoleMessage('[Squeezelite] Could not write the script with error: ' + err);
+				defer.reject(new Error(err));
+			}
+			else 
+			{
+				defer.resolve();
+			}
         });
 	});
-	
 	return defer.promise;
 };
