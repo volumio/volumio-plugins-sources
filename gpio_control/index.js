f448b99c9db0ab9e8b954fd57468cddbb8c00239
Bill Krahmer <billism@gmail.com>
2022-05-24 21:41:21 -0500

    Reverting changes to PIOControl.prototype.handleEvent = function(e)

--- gpio_control/index.js
+++ gpio_control/index.js
@@ -342,13 +342,12 @@ GPIOControl.prototype.handleEvent = function(e) {
 	var self = this;
 
 	self.GPIOs.forEach(function(gpio) {
+		if (gpio.e == e){
+
 			// Clear any previous timers
 			clearTimeout(gpio.delayTimeoutId);
 			clearTimeout(gpio.durationTimeoutId);
-	});
 
-	self.GPIOs.forEach(function(gpio) {
-		if (gpio.e == e){
 			self.log(`*** ${e} ***`);
 			self.log(`Delaying: ${gpio.delay}ms`);
 
