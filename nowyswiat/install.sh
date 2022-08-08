7cfa34b701ca4e6d0179b1f9ebc02ebe4ccf4bcd
Jacek Domanski <djack@djack.com.pl>
2022-08-08 13:47:28 +0200

    Plugin Radio 357
    Plugin Radio 357

--- nowyswiat/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Installing nowyswiat Dependencies"
-
-#requred to end the plugin install
-echo "plugininstallend"
--- radio357/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Installing radio 357 Dependencies"
-
-#requred to end the plugin install
-echo "plugininstallend"
