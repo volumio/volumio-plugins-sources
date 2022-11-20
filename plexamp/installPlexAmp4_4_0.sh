be80650be62cfdce5d7a15223bd079b6eb00ba12
Jerome O'Flaherty <jerome.oflaherty@gmail.com>
2022-11-20 14:20:19 +0000

    Made the output of script goto systemd cat

--- plexamp/installPlexAmp4_4_0.sh
+++ plexamp/installPlexAmp4_4_0.sh
@@ -9,12 +9,12 @@ fi
 apt-get -qqy install expect
 
 # Download and extract plexamp 4_4
-echo "Download plexamp..."
+echo "Download plexamp..."  | systemd-cat
 wget https://plexamp.plex.tv/headless/Plexamp-Linux-headless-v4.4.0.tar.bz2
 tar -xf Plexamp-Linux-headless-v4.4.0.tar.bz2
 
 # 4_4 needs node 12 but volumio has 14 so lets download nvm first
-echo "Installing NVM..."
+echo "Installing NVM..."  | systemd-cat
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # export NVM so it can be usable below
@@ -22,25 +22,25 @@ export NVM_DIR=$HOME/.nvm;
 source $NVM_DIR/nvm.sh;
 
 # install node 12 for plexamp
-echo "Installing Node 12 for older Plexamp..."
+echo "Installing Node 12 for older Plexamp..."  | systemd-cat
 nvm install 12
 
 # but make sure we use the system one by default
 nvm use system
 
 # install or custom plex
-echo "Copying plexamp service..."
+echo "Copying plexamp service..."  | systemd-cat
 cp plexamp.service /etc/systemd/system/plexamp.service
 
-echo "Activating plexamp ..."
+echo "Activating plexamp with claim token $1 ..." | systemd-cat
 h=$(hostname -f)
 chmod +x activatePlexAmp.exp
 ./activatePlexAmp.exp $1 $h
 
 # enable the plexamp.service
-echo "Enabling plexamp ..."
+echo "Enabling plexamp ..." | systemd-cat
 
 systemctl enable plexamp.service
 systemctl start plexamp.service
 
-echo "Headless PlexAmp installed"
+echo "Headless PlexAmp installed" | systemd-cat
