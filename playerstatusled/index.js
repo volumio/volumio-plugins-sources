7545e8bfc86947bff8348068b234b9949d3d2e7c
andrewpono <rogerkrolik@yahoo.com>
2023-01-19 20:44:45 +0000

    Added websocket connection error handling

--- playerstatusled/index.js
+++ playerstatusled/index.js
@@ -22,55 +22,57 @@ function StatusLedController(context) {
 }
 
 StatusLedController.prototype.onVolumioStart = function () {
-    var self = this;
+	var self = this;
 	
-    var configFile = self.commandRouter.pluginManager.getConfigurationFile(self.context, 'config.json');
+	var configFile = self.commandRouter.pluginManager.getConfigurationFile(self.context, 'config.json');
 	self.config = new (require('v-conf'))(); // this needs to be here to load config, not in global var declarations.
-    self.config.loadFile(configFile);
+	self.config.loadFile(configFile);
 	self.log('Initialized');
 	
-    return libQ.resolve();
+	return libQ.resolve();
 };
 
 StatusLedController.prototype.onStart = function () {
-    var self = this;
-    var defer = libQ.defer();
+	var self = this;
+	var defer = libQ.defer();
 
 	self.load18nStrings();
 	socket = io.connect('http://localhost:3000');
+	socket.on('connect_error', (err) => {
+		self.log('websocket connection error: ' + err);
+		defer.reject();
+	});
+	
+	socket.on('connect', () => {
+		self.log('connected to websocket');
+		socket.on('error', (err) => {self.log('websocket error: ' + err)}); // handle possible socket errors
+		socket.on('pushState', self.statusChanged.bind(self)); // listen to status reports from Volumio websocket
+		self.initLed();
+		socket.emit('getState', ''); // make socket push status update on startup to reflect current state
+		defer.resolve();
+	});
 
-    self.initLed();
-
-	// read and parse status once
-	socket.emit('getState', '');
-	socket.once('pushState', self.statusChanged.bind(self));
-
-	// listen to every subsequent status report from Volumio
-	socket.on('pushState', self.statusChanged.bind(self));
-
-    // Once the Plugin has successfully started resolve the promise
-    defer.resolve();
-    return defer.promise;
+	return defer.promise;
 };
 
 StatusLedController.prototype.onStop = function () {
-    var self = this;
+	var self = this;
 
 	if(led?.timerId) {
 		clearInterval(led.timerId);
 	}
-        
+		
 	socket.off('pushState'); // socket.off() removes all listeners
 	socket.disconnect();
 
-    self.releaseLed();
+	self.releaseLed();
 
-    return libQ.resolve();
+	return libQ.resolve();
 };
 
 // Player status has changed (might not always be play or pause action)
 StatusLedController.prototype.statusChanged = function (state) {
-    var self = this;
+	var self = this;
 
 	//self.log(`player state update: ${led.playerState} -> ${state.status}`); // DEBUGGING only
 
@@ -81,18 +83,18 @@ StatusLedController.prototype.statusChanged = function (state) {
 
 	self.log(`player state update: ${led.playerState} -> ${state.status}`);
 
-    switch (state.status) {
-        case 'play':
-            self.log('turning LED on');
+	switch (state.status) {
+		case 'play':
+			self.log('turning LED on');
 			if(led.timerId) {
 				clearInterval(led.timerId); // stop blinking before turning LED on
 			}
 			led.write(1)
 				.catch(err => self.log(err));
-            break;
-        case 'pause': // BUG: volumio allows pause command on web streams. Resuming with play does not produce status update. Exit-stop.
+			break;
+		case 'pause': // BUG: volumio allows pause command on web streams. Resuming with play does not produce status update. Exit-stop.
 			self.log('starting to blink LED');
-            led.timerId = setInterval( function() {
+			led.timerId = setInterval( function() {
 				led.read()
 					.then(value => led.write(value ^ 1))
 					.catch(err => {
@@ -100,10 +102,10 @@ StatusLedController.prototype.statusChanged = function (state) {
 						clearInterval(led.timerId); // plugin may be disabled or GPIO unexported during blinking.
 					})
 				}, led.delay);
-            break;
-        default:
-            self.log('turning LED off');
-            if(led.timerId) {
+			break;
+		default:
+			self.log('turning LED off');
+			if(led.timerId) {
 				clearInterval(led.timerId);
 			}
 			led.write(0)
@@ -115,10 +117,10 @@ StatusLedController.prototype.statusChanged = function (state) {
 
 // initialize LED gpio to the one stored in the config
 StatusLedController.prototype.initLed = function() {
-    var self = this;
+	var self = this;
 
-    self.log('initializing GPIO');
-    led = new gpio(self.config.get('gpionum'), 'out');
+	self.log('initializing GPIO');
+	led = new gpio(self.config.get('gpionum'), 'out');
 	led.timerId; // store blinker timer reference here
 	led.playerState; // player state before state change.
 	led.delay = self.config.get('interval');	// blink interval
@@ -127,14 +129,14 @@ StatusLedController.prototype.initLed = function() {
 
 // initialize LED gpio to the one stored in the config
 StatusLedController.prototype.releaseLed = function() {
-    var self = this;
+	var self = this;
 
-    self.log('releasing GPIO');
+	self.log('releasing GPIO');
 	try {
-        led.unexport(); // something else may have unexported this GPIO, fatal error. Unexport is synchronous.
-    } catch(err) {
+		led.unexport(); // something else may have unexported this GPIO, fatal error. Unexport is synchronous.
+	} catch(err) {
 		self.log('releasing GPIO: ' + err);
-    }
+	}
 };
 
 // Output to log
@@ -146,8 +148,8 @@ StatusLedController.prototype.log = function(s) {
 // Settings Methods -----------------------------------------------------------------------------
 
 StatusLedController.prototype.saveSettings = function (data) {
-    var self = this;
-    var defer = libQ.defer();
+	var self = this;
+	var defer = libQ.defer();
 
 	try {	
 		if (isNaN(data['gpionum']) || data['gpionum'] < 0 || data['gpionum'] > 200) {
@@ -171,61 +173,61 @@ StatusLedController.prototype.saveSettings = function (data) {
 	
 		self.commandRouter.pushToastMessage('success', self.getI18nString('SUCCESS_TITLE'), self.getI18nString('SUCCESS_MESSAGE'));
 
-    } catch(err) { // some errors require toString() to be properly displayed by the toast msg.
+	} catch(err) { // some errors require toString() to be properly displayed by the toast msg.
 		self.commandRouter.pushToastMessage('error', self.getI18nString('ERROR_MESSAGE'), err.toString());
-    }
+	}
 
-    defer.resolve();
+	defer.resolve();
 
-    return defer.promise;
+	return defer.promise;
 };
 
 StatusLedController.prototype.load18nStrings = function () {
-    var self = this;
+	var self = this;
 
-    try {
-        var language_code = this.commandRouter.sharedVars.get('language_code');
-        self.i18nStrings = fs.readJsonSync(__dirname + '/i18n/strings_' + language_code + '.json');
-    } catch (e) {
-        self.i18nStrings = fs.readJsonSync(__dirname + '/i18n/strings_en.json');
-    }
+	try {
+		var language_code = this.commandRouter.sharedVars.get('language_code');
+		self.i18nStrings = fs.readJsonSync(__dirname + '/i18n/strings_' + language_code + '.json');
+	} catch (e) {
+		self.i18nStrings = fs.readJsonSync(__dirname + '/i18n/strings_en.json');
+	}
 
-    self.i18nStringsDefaults = fs.readJsonSync(__dirname + '/i18n/strings_en.json');
+	self.i18nStringsDefaults = fs.readJsonSync(__dirname + '/i18n/strings_en.json');
 };
 
 StatusLedController.prototype.getI18nString = function (key) {
-    var self = this;
+	var self = this;
 
-    if (self.i18nStrings[key] !== undefined)
-        return self.i18nStrings[key];
-    else
-        return self.i18nStringsDefaults[key];
+	if (self.i18nStrings[key] !== undefined)
+		return self.i18nStrings[key];
+	else
+		return self.i18nStringsDefaults[key];
 };
 
 // Configuration Methods -----------------------------------------------------------------------------
 StatusLedController.prototype.getUIConfig = function () {
-    var self = this;
+	var self = this;
 	var defer = libQ.defer();
 
-    const lang_code = this.commandRouter.sharedVars.get('language_code');
+	const lang_code = this.commandRouter.sharedVars.get('language_code');
 
-    self.commandRouter.i18nJson(__dirname+'/i18n/strings_'+lang_code+'.json',
-        __dirname+'/i18n/strings_en.json',
-        __dirname + '/UIConfig.json')
-        .then(function(uiconf)
-        {
+	self.commandRouter.i18nJson(__dirname+'/i18n/strings_'+lang_code+'.json',
+		__dirname+'/i18n/strings_en.json',
+		__dirname + '/UIConfig.json')
+		.then(function(uiconf)
+		{
 			uiconf.sections[0].content[0].value = self.config.get('gpionum', 22);
 			uiconf.sections[0].content[1].value = self.config.get('activestate', 0);
-            uiconf.sections[0].content[2].value = self.config.get('interval', 500);
-            defer.resolve(uiconf);
-        })
-        .fail(function(err)
-        {
-            self.logger.error(`Failed to parse UI Configuration page for plugin ${pluginName}: ${err}`); 
+			uiconf.sections[0].content[2].value = self.config.get('interval', 500);
+			defer.resolve(uiconf);
+		})
+		.fail(function(err)
+		{
+			self.logger.error(`Failed to parse UI Configuration page for plugin ${pluginName}: ${err}`); 
 			defer.reject(err);
-        });
+		});
 
-    return defer.promise;
+	return defer.promise;
 };
 
 StatusLedController.prototype.getConfigurationFiles = function () {
@@ -233,11 +235,11 @@ StatusLedController.prototype.getConfigurationFiles = function () {
 };
 
 StatusLedController.prototype.updateUIConfig = function () {
-  var self = this;
+	var self = this;
 
-  self.commandRouter.getUIConfigOnPlugin('system_hardware', 'playerstatusled', {})
-    .then(function (uiconf) {
-      self.commandRouter.broadcastMessage('pushUiConfig', uiconf);
-    });
-  self.commandRouter.broadcastMessage('pushUiConfig');
+	self.commandRouter.getUIConfigOnPlugin('system_hardware', 'playerstatusled', {})
+	.then(function (uiconf) {
+		self.commandRouter.broadcastMessage('pushUiConfig', uiconf);
+	});
+	self.commandRouter.broadcastMessage('pushUiConfig');
 };
