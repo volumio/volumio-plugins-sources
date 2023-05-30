0661770e85e6b66db434ebf09fe1fccaf5b52657
Pietro Diana <pietro.diana@dmd2.com>
2023-05-30 15:35:48 +0200

    fix plugin_name

--- dmd2_music/index.js
+++ dmd2_music/index.js
@@ -198,7 +198,7 @@ ControllerHotelRadio.prototype.addToBrowseSources = function () {
     var self = this;
 
     self.logger.info('Adding Hotel Radio to Browse Sources');
-    var data = {name: 'hotelradio.fm', uri: 'hotelradio://',plugin_type:'music_service',plugin_name:'hotelradio',albumart:'/albumart?sectionimage=music_service/dmd2_music/icons/dmd2_music-icon.png'};
+    var data = {name: 'hotelradio.fm', uri: 'hotelradio://',plugin_type:'music_service',plugin_name:'dmd2_music',albumart:'/albumart?sectionimage=music_service/dmd2_music/icons/dmd2_music-icon.png'};
     return self.commandRouter.volumioAddToBrowseSources(data);
 }
 
