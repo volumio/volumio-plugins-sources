77ec15e0c0794b80b80c9d285365b9782993fb9e
chourmovs <chourmovs@gmail.com>
2024-02-02 19:02:20 +0100

    a

--- moode/Volumiomod.sh
+++ moode/Volumiomod.sh
@@ -3,4 +3,5 @@
 echo "volumio" | sudo -S --prompt="" systemctl stop mpd.service mpd.socket nfs-client.target smbd.service
 echo "volumio" | sudo -S --prompt="" systemctl disable mpd.service mpd.socket nfs-client.target smbd.service
 echo "volumio" | sudo -S --prompt="" systemctl mask mpd.service mpd.socket nfs-client.target smbd.service
+echo "volumio" | sudo -S --prompt="" docker exec -ti debian-moode /bin/bash apt reinstall moode-player --fix-missing
 echo "volumio" | sudo -S --prompt="" docker restart $(docker ps -a -q)
