7d0d74dc6c18caaafd7c79610040dd7d474275ea
7h0mas-R <github@thomas-rohleder.de>
2024-02-12 23:43:24 +0100

    merged two dev branches

--- rotaryencoder2/index.js
+++ rotaryencoder2/index.js
@@ -956,6 +956,7 @@ rotaryencoder2.prototype.detachListener = function (handle){
         	defer.resolve();
         } else {
             self.logger.error('[ROTARYENCODER2] detachListener: could not kill handler process ' + handle);
+			self.commandRouter.pushToastMessage('errpr', self.getI18nString('ROTARYENCODER2.TOAST_WRONG_PARAMETER'), self.getI18nString('ROTARYENCODER2.TOAST_KILL_HANDLE_FAIL'));
             defer.resolve();
         }
 
