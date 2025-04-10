b3e0dd085ca689fc24403fe18663c7d4c6e4b75c
Paweł Gorgoń <pgorgon@hem-e.com>
2025-04-10 21:13:41 +0200

    Added udev rules for all usb devices in order service can find FSCT devices and write to them

--- ferrum_streaming_control_technology/install.sh
+++ ferrum_streaming_control_technology/install.sh
@@ -5,9 +5,16 @@ sudo apt-get update
 # Install the required packages via apt-get
 sudo apt-get -y install
 
+# probably needs to install libssl-dev, but I'm not sure
+sudo cp 9-fsct-usb-devices.rules /etc/udev/rules.d/
+sudo udevadm control -R 
+sudo udevadm trigger
+
 # If you need to differentiate install for armhf and i386 you can get the variable like this
 #DPKG_ARCH=`dpkg --print-architecture`
 # Then use it to differentiate your install
+
+# here copy different bin file?
 
 #requred to end the plugin install
 echo "plugininstallend"
--- ferrum_streaming_control_technology/uninstall.sh
+++ ferrum_streaming_control_technology/uninstall.sh
@@ -3,5 +3,9 @@
 # Uninstall dependendencies
 # apt-get remove -y
 
+sudo rm -rf /etc/udev/rules.d/9-fsct-usb-devices.rules
+sudo udevadm control -R
+sudo udevadm trigger
+
 echo "Done"
 echo "pluginuninstallend"
