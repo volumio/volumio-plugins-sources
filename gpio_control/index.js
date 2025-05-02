28cdfc1f5b21ed7b898250caf80732ebf2010740
balbuze <balbuze@gmail.com>
2025-05-02 12:22:26 +0200

    Revert "gpio_control - bookworm version"
    This reverts commit b61d021413eb8166c20ee8bd7e50c58de17c574a.

--- gpio_control/index.js
+++ gpio_control/index.js
@@ -1,16 +1,7 @@
-// Quick commands:
-//
-// volumio plugin refresh && volumio vrestart
-// journalctl -f  
-
-// Oh fucko, don't forget config.json files get cached at: /data/configuration/system_hardware/gpio_control
 'use strict';
 
-// Disable this 
-const logging = false;
-
-// How often to check to generate network events
-const defaultNetworkCheckInterval = 5;
+// Logging: sudo journalctl -f
+const logging = true;
 
 // I used tomatpasser's gpio-buttons plugin as a basis for this project
 const libQ = require("kew");
@@ -21,7 +12,6 @@ const io = require('socket.io-client');
 const sleep = require('sleep');
 const socket = io.connect("http://localhost:3000");
 const execSync = require('child_process').execSync;
-// var exec = require('child_process').exec;
 
 // gpiox library
 // https://www.npmjs.com/package/@iiot2k/gpiox?activeTab=readme
@@ -57,15 +47,7 @@ const EVENT = {
 	REPEAT_ALL_ON: "repeatAllOn",
 	REPEAT_ALL_OFF: "repeatAllOff",
 	REPEAT_SINGLE_ON: "repeatSingleOn",
-	REPEAT_SINGLE_OFF: "repeatSingleOff",
-	WIFI_CONNECTED: "wifiConnected",
-	WIFI_DISCONNECTED: "wifiDisconnected",
-	ETHERNET_CONNECTED: "ethernetConnected",
-	ETHERNET_DISCONNECTED: "ethernetDisconnected",
-	HOST1_ONLINE: "host1Online",
-	HOST1_OFFLINE: "host1Offline",
-	HOST2_ONLINE: "host2Online",
-	HOST2_OFFLINE: "host2Offline",
+	REPEAT_SINGLE_OFF: "repeatSingleOff"	
 };
 
 // Events that we can detect and do something
@@ -84,22 +66,7 @@ const events = [
 	EVENT.REPEAT_ALL_OFF, 
 	EVENT.REPEAT_SINGLE_ON, 
 	EVENT.REPEAT_SINGLE_OFF,
-	EVENT.REPEAT_OFF,
-	EVENT.WIFI_CONNECTED,
-	EVENT.WIFI_DISCONNECTED,
-	EVENT.ETHERNET_CONNECTED,
-	EVENT.ETHERNET_DISCONNECTED,
-	EVENT.HOST1_ONLINE,
-	EVENT.HOST1_OFFLINE,
-	EVENT.HOST2_ONLINE,
-	EVENT.HOST2_OFFLINE
-];
-
-const hostEvents = [
-	EVENT.HOST1_ONLINE, 
-	EVENT.HOST1_OFFLINE, 
-	EVENT.HOST2_ONLINE, 
-	EVENT.HOST2_OFFLINE
+	EVENT.REPEAT_OFF
 ];
 
 module.exports = GPIOControl;
@@ -114,9 +81,6 @@ function GPIOControl(context) {
 	self.load18nStrings();
 	self.GPIOs = [];
 	self.previousState = "";
-	self.initialNetworkChecks = true;
-	self.networkChecks = {};
-	self.networkTimer = 0;
 }
 
 // Volumio is starting
@@ -166,8 +130,6 @@ GPIOControl.prototype.onStart = function() {
 		defer.resolve();
 	});
 
-	self.startNetworkEventsMonitoring();
-
 	return defer.promise;
 };
 
@@ -175,8 +137,6 @@ GPIOControl.prototype.onStart = function() {
 GPIOControl.prototype.onStop = function() {
 	const self = this;
 	const defer = libQ.defer();
-
-	self.stopNetworkEventsMonitoring();
 
 	self.clearGPIOs()
 	.then ((result) => {
@@ -229,24 +189,26 @@ GPIOControl.prototype.getUIConfig = function() {
 			const configDelayUnits = e.concat(".delayUnits");
 			const configDuration = e.concat(".duration");
 			const configDurationUnits = e.concat(".durationUnits");
-			const configHost = e.concat(".host");
+
+			// Extend the find method on the content array - mental but works
+			uiconf.sections[0].content.findItem = function(obj) {
+				return this.find(function(item) {
+					for (const prop in obj)
+						if (!(prop in item) || obj[prop] !== item[prop])
+							return false;
+					return true;
+				});
+			}
 
 			// Populate our controls
 			self.setSwitchElement(uiconf, enabled, config.get(configEnabled)); // event on/off
-			self.setSelectElement(uiconf, pin, config.get(configPin)); // GPIO pin
-			self.setSelectElement(uiconf, state, config.get(configState)); // state
-			self.setSelectElement(uiconf, delay, config.get(configDelay)); // delay
-			self.setSelectElement(uiconf, delayUnits, config.get(configDelayUnits)); // delay units
-			self.setSelectElement(uiconf, duration, config.get(configDuration)); // duration
-			self.setSelectElement(uiconf, durationUnits, config.get(configDurationUnits)); // duration units
-
-			// Populate host if required
-			if (hostEvents.includes(e)) {
-				self.setTextElement(uiconf, `${e}Host`, config.get(configHost));
-			}
+			self.setSelectElementStr(uiconf, pin, config.get(configPin)); // GPIO pin
+			self.setSelectElement(uiconf, state, config.get(configState), self.boolToString(config.get(configState))); // state
+			self.setSelectElement(uiconf, delay, config.get(configDelay), self.delayToString(config.get(configDelay))); // delay
+			self.setSelectElement(uiconf, delayUnits, config.get(configDelayUnits), self.unitsToString(config.get(configDelayUnits))); // delay units
+			self.setSelectElement(uiconf, duration, config.get(configDuration), self.durationToString(config.get(configDuration))); // duration
+			self.setSelectElement(uiconf, durationUnits, config.get(configDurationUnits), self.unitsToString(config.get(configDurationUnits))); // duration units
 		});
-
-		self.setSelectElement(uiconf, "networkCheckInterval", config.get("networkCheck.interval") || defaultNetworkCheckInterval);
 
 		defer.resolve(uiconf);
 	})
@@ -262,7 +224,6 @@ GPIOControl.prototype.getUIConfig = function() {
 GPIOControl.prototype.saveConfig = function(data){
 	const self = this;
 
-	self.stopNetworkEventsMonitoring();
 	self.clearGPIOs();
 
 	// Loop through standard events
@@ -276,7 +237,6 @@ GPIOControl.prototype.saveConfig = function(data){
 		const delayUnits = item.concat("DelayUnits");
 		const duration = item.concat("Duration");
 		const durationUnits = item.concat("DurationUnits");
-		const host = item.concat("Host");
 
 		// Save config
 		config.set(item.concat(".enabled"), data[enabled]);
@@ -286,13 +246,7 @@ GPIOControl.prototype.saveConfig = function(data){
 		config.set(item.concat(".delayUnits"), data[delayUnits]["value"]);
 		config.set(item.concat(".duration"), data[duration]["value"]);
 		config.set(item.concat(".durationUnits"), data[durationUnits]["value"]);
-
-		if (hostEvents.includes(item)) {
-			config.set(item.concat(".host"), data[host]);
-		}
 	});
-
-	config.set("networkCheck.interval", data["networkCheckInterval"]["value"]);
 
 	// Clear any previous states
 	self.previousState = "";
@@ -305,9 +259,6 @@ GPIOControl.prototype.saveConfig = function(data){
 
 	// Retrieve playing status
 	socket.emit("getState", "");
-
-	// Start network monitoring
-	self.startNetworkEventsMonitoring();
 
 	// Display toaster message
 	self.commandRouter.pushToastMessage("success", self.getI18nString("PLUGIN_CONFIGURATION"), self.getI18nString("SETTINGS_SAVED"));
@@ -335,14 +286,8 @@ GPIOControl.prototype.createGPIOs = function() {
 		const delayUnits = self.unitsToString(config.get(configDelayUnits));
 		const duration = self.getDurationMs(config.get(configDuration), config.get(configDurationUnits));
 		const durationUnits = self.unitsToString(config.get(configDurationUnits));
-		var host = "";
 
 		if (enabled){
-
-			if (hostEvents.includes(e)) {
-				host = " (" + config.get(`${e}.host`) + ") ";
-			}
-
 			var msg = `On ${e} will set GPIO ${pin} to ${self.boolToString(state)}`;
 
 			if (delay > 0){
@@ -405,7 +350,6 @@ GPIOControl.prototype.clearGPIOs = function () {
 };
 
 // Playing status has changed, more than one thing might have changed
-// Undefined status is also OK
 // https://volumio.github.io/docs/API/REST_API.html
 GPIOControl.prototype.statusChanged = function(state) {
 	const self = this;
@@ -507,8 +451,7 @@ GPIOControl.prototype.handleEvent = function(e) {
 				}
 			});
 
-			self.logHeading(e);
-
+			self.log(`*** ${e} ***`);
 			if (gpio.delay > 0){
 				self.log(`Delaying: ${gpio.delay}ms`);
 			}
@@ -551,19 +494,25 @@ GPIOControl.prototype.log = function(s) {
 	self.logger.info(`[GPIO_Control] ${s}`);
 }
 
-// Output to log with a big fat heading
-GPIOControl.prototype.logHeading = function(s) {
-	const self = this;
-	self.log("#".repeat(s.length + 4));
-	self.log(`# ${s} #`);
-	self.log("#".repeat(s.length + 4));
-}
-
 // Function for printing booleans
 GPIOControl.prototype.boolToString = function(value){
 	const self = this;
 
 	return value ? self.getI18nString("ON") : self.getI18nString("OFF");
+}
+
+// Function for retrieving duration
+GPIOControl.prototype.durationToString = function(value){
+	const self = this;
+
+	return value == 0 ? self.getI18nString("DURATION_NONE") : value;
+}
+
+// Funciton for retrieving delay
+GPIOControl.prototype.delayToString = function(value){
+	const self = this;
+
+	return value == 0 ? self.getI18nString("DELAY_NONE") : value;
 }
 
 // Function for displaying units
@@ -609,15 +558,11 @@ GPIOControl.prototype.getI18nString = function (key) {
 };
 
 // Retrieve a UI element from UI config
-GPIOControl.prototype.getUIElement = function(obj, id){
+GPIOControl.prototype.getUIElement = function(obj, field){
 	const self = this;
-	return obj.sections[0].content.find(element => element.id === id);
-}
+	const lookfor = JSON.parse('{"id":"' + field + '"}');
 
-// Populate a text element
-GPIOControl.prototype.setTextElement = function(obj, field, value){
-	const self = this;
-	self.setSwitchElement(obj, field, value);
+	return obj.sections[0].content.findItem(lookfor);
 }
 
 // Populate switch UI element
@@ -625,210 +570,30 @@ GPIOControl.prototype.setSwitchElement = function(obj, field, value){
 	const self = this;
 	const result = self.getUIElement(obj, field);
 	
-	if (!result){
-		self.log(`Could not find control ${field}`);
-	}
-
-	result.value = value;
-}
-
-// Populate select UI element
-GPIOControl.prototype.setSelectElement = function(obj, field, value){
-	const self = this;
-	const result = self.getUIElement(obj, field);
-	if (!result){
-		self.log(`Could not find control ${field}`);
-	}
-
-	const itemInList = result.options.find(opt => opt.value === value)
-	if (!itemInList){
-		self.log(`Could not find item in list with value ${value} for control ${field}`);
-	}
-	
-	result.value.value = itemInList.value;
-	result.value.label = itemInList.label;
-}
-
-// Start checking for network events
-GPIOControl.prototype.startNetworkEventsMonitoring = function() {
-	const self = this;
-
-	if (
-		!config.get("wifiConnected.enabled") && !config.get("wifiDisconnected.enabled") &&
-		!config.get("ethernetConnected.enabled") && !config.get("ethernetDisconnected.enabled") && 
-		!config.get("host1Online.enabled") && !config.get("host1Offline.enabled") &&
-		!config.get("host2Online.enabled") && !config.get("host2Offline.enabled")
-	){
-		self.log(`No networks events to be monitored`);
+	if (result){
+		result.value = value;
 		return;
 	}
 
-	self.networkChecks = {
-		wifiConnected: false,
-		ethernetConnected: false,
-		host1Online: false,
-		host1Offline: false,
-		host2Online: false,
-		host2Offline: false
-	}
-
-	const interval = config.get("networkCheck.interval") || defaultNetworkCheckInterval;
-	self.networkTimer = setInterval(() => self.networkEvents(), interval * 1000);
-	self.log(`Starting network event monitoring, every ${interval} seconds`);
+	self.log(`Could not find control ${field}`);
 }
 
-// Stop checking network events
-GPIOControl.prototype.stopNetworkEventsMonitoring = function() {
+// Populate select UI element
+GPIOControl.prototype.setSelectElement = function(obj, field, value, label){
 	const self = this;
-	self.log("Stopping network event monitoring");
-	clearInterval(self.networkTimer);
+	const result = self.getUIElement(obj, field);
+	
+	if (result){
+		result.value.value = value;
+		result.value.label = label;
+		return;
+	}
+
+	self.log(`Could not find control ${field}`);
 }
 
-// Do network events checks
-GPIOControl.prototype.networkEvents = function() {
+// Populate select UI element when value matches the label
+GPIOControl.prototype.setSelectElementStr = function(obj, field, value){
 	const self = this;
-
-	self.log("Checking network events");
-
-	// Wifi connection
-	var wifiConnected;
-	if (config.get("wifiConnected.enabled") || config.get("wifiDisconnected.enabled")){
-		wifiConnected = self.wifiConnected();
-	}
-
-	if (config.get("wifiConnected.enabled") && wifiConnected){
-		if (self.initialNetworkChecks || !self.initialNetworkChecks && !self.networkChecks.wifiConnected){
-			self.handleEvent(EVENT.WIFI_CONNECTED)
-		}
-	}
-	if (config.get("wifiDisconnected.enabled") && !wifiConnected){
-		if (self.initialNetworkChecks || !self.initialNetworkChecks && self.networkChecks.wifiConnected){
-			self.handleEvent(EVENT.WIFI_DISCONNECTED);
-		}
-	}
-
-	// Ethernet connected
-	var ethernetConnected;
-	if (config.get("ethernetConnected.enabled") || config.get("ethernetDisconnected.enabled")){
-		ethernetConnected = self.ethernetConnected();
-	}
-
-	if (config.get("ethernetConnected.enabled") && ethernetConnected){
-		if (self.initialNetworkChecks || !self.initialNetworkChecks && !self.networkChecks.ethernetConnected){
-			self.handleEvent(EVENT.ETHERNET_CONNECTED);
-		}
-	}
-	if (config.get("ethernetDisconnected.enabled") && !ethernetConnected){
-		if (self.initialNetworkChecks || !self.initialNetworkChecks && self.networkChecks.ethernetConnected){
-			self.handleEvent(EVENT.ETHERNET_DISCONNECTED);
-		}
-	}
-
-	// Host 1
-	var host1Online = false;
-	if (config.get("host1Online.enabled") && config.get("host1Online.host").trim() != ""){
-		host1Online = self.hostOnline(config.get("host1Online.host"));
-		if (host1Online){
-			if (self.initialNetworkChecks || !self.initialNetworkChecks && !self.networkChecks.host1Online){
-				self.handleEvent(EVENT.HOST1_ONLINE);
-			}
-		}
-	}
-	var host1Offline = false;
-	if (config.get("host1Offline.enabled") && config.get("host1Offline.host").trim() != ""){
-		host1Offline = !self.hostOnline(config.get("host1Offline.host"));
-		if (host1Offline){
-			if (self.initialNetworkChecks || !self.initialNetworkChecks && !self.networkChecks.host1Offline){
-				self.handleEvent(EVENT.HOST1_OFFLINE);
-			}
-		}
-	}
-
-	// Host 2
-	var host2Online = false;
-	if (config.get("host2Online.enabled") && config.get("host2Online.host").trim() != ""){
-		host2Online = self.hostOnline(config.get("host2Online.host"));
-		if (host2Online){
-			if (self.initialNetworkChecks || !self.initialNetworkChecks && !self.networkChecks.host2Online){
-				self.handleEvent(EVENT.HOST2_ONLINE);
-			}
-		}
-	}
-
-	var host2Offline = false;
-	if (config.get("host2Offline.enabled") && config.get("host2Offline.host").trim() != ""){
-		host2Offline = !self.hostOnline(config.get("host2Offline.host"));
-		if (host2Offline){
-			if (self.initialNetworkChecks || !self.initialNetworkChecks && !self.networkChecks.host2Offline){
-				self.handleEvent(EVENT.HOST2_OFFLINE);
-			}
-		}
-	}
-	
-	self.networkChecks = {
-		wifiConnected: wifiConnected,
-		ethernetConnected: ethernetConnected,
-		host1Online: host1Online,
-		host1Offline: host1Offline,
-		host2Online: host2Online,
-		host2Offline: host2Offline
-	};
-
-	self.initialNetworkChecks = false;
-};
-
-// Are we connected to wifi?
-GPIOControl.prototype.wifiConnected = function() {
-	const self = this;
-	const command = "/sbin/iw dev wlan0 link";
-	
-	try{
-		const buffer = execSync(command).toString();
-		// Look for not connected
-		return !/not connected/i.test(buffer);
-	}
-	catch(e){
-		const error = e.toString();
-		self.log(`Error checking wifi connection: ${error}`);
-	}
-	return false;
-};
-
-// Are we connected to the ethernet?
-GPIOControl.prototype.ethernetConnected = function() {
-	const self = this;
-	const command = "cat /sys/class/net/eth0/carrier";
-
-	try{
-		const buffer = execSync(command).toString();
-		// Command returns either a 1 or 0
-		return buffer.includes("1");
-	}
-	catch(e){
-		const error = e.toString();
-		self.log(`Error checking ethernet connection: ${error}`);
-	}
-	return false;
-};
-
-// Are we connected to the ethernet?
-GPIOControl.prototype.hostOnline = function(host) {
-	const self = this;
-	const packets = 2;
-	const command = `ping -c ${packets} ${host}`;
-
-	try{
-		const buffer = execSync(command).toString();
-		// Look for the packetloss %age - if device is offline then command just returns an error
-		const match = buffer.match(/(\d+)% packet loss/);
-		if (!match){
-			return false;
-		}
-		const packetLoss = parseInt(match[1], 10);
-		return packetLoss != 100; // If all packets lost then device is offline
-	}
-	catch(e){
-		// this occurs if the host can't be found
-	}
-	return false;
-};
+	self.setSelectElement(obj, field, value, value.toString());
+}
