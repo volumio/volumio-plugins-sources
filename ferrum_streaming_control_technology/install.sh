7e0e57cac9563fb50205bfb8804acc88e116d73d
Paweł Gorgoń <pgorgon@hem-e.com>
2025-05-06 18:56:22 +0200

    Removed udev rules, because they will be added into volumio3-os. Restored arm64 architecture

--- ferrum_streaming_control_technology/install.sh
+++ ferrum_streaming_control_technology/install.sh
@@ -5,13 +5,6 @@ echo "Installing ferrum streaming control technology Dependencies"
 # Install the required packages via apt-get
 # sudo apt-get -y install
 
-# sudo cp 09-usb-devices-plugdev.rules /lib/udev/rules.d/
-# sudo udevadm control -R 
-# sudo udevadm trigger
-
-dpkg-deb --build fsct-usb-plugdev-udev-rules
-sudo dpkg -i fsct-usb-plugdev-udev-rules.deb
-
 # If you need to differentiate install for armhf and i386 you can get the variable like this
 #DPKG_ARCH=`dpkg --print-architecture`
 # Then use it to differentiate your install
--- ferrum_streaming_control_technology/uninstall.sh
+++ ferrum_streaming_control_technology/uninstall.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-sudo dpkg -r fsct-usb-plugdev-udev-rules.deb
-
 echo "Done"
 echo "pluginuninstallend"
